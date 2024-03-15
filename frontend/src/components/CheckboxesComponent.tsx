1b89940f2480bafeee027b676ec7b03cdfd70515
Yana Avetisyan <yanagithuby@gmail.com>
2024-03-15 16:55:22 +0300

    add drag and drow in checkbox

--- frontend/src/components/CheckboxesComponent.tsx
+++ frontend/src/components/CheckboxesComponent.tsx
@@ -2,26 +2,34 @@ import React from 'react';
 import { Button, FormControlLabel, FormGroup, Input } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 import useList from '../hooks/UseList';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-interface CheckboxesComponentProps{
+interface CheckboxesComponentProps {
   cardIndex?: number;
   updateCardAnswers?: (index: number, answers: string[]) => void;
   disabled: boolean;
   answers?: string[];
 }
 
-const CheckboxesComponent: React.FC<CheckboxesComponentProps> = ({ 
+const CheckboxesComponent: React.FC<CheckboxesComponentProps> = ({
   cardIndex,
   updateCardAnswers,
   disabled = false,
   answers = [],
- }) => {
+}) => {
   const { list, addItem, updateItem } = useList<string[]>([['']]);
 
   const handleAddAnswer = () => {
     addItem(['']);
   };
-  
+
+  const handleDragEnd = (result: any) => {
+    if (!result.destination) return;
+    const items = [...list];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+   };
+
   //Добавляем ответы
   const handleUpdateAnswer = (index: number, value: string) => {
     const newAnswers = [...list];
@@ -34,41 +42,53 @@ const CheckboxesComponent: React.FC<CheckboxesComponentProps> = ({
 
   return (
     <FormGroup sx={{ width: '-webkit-fill-available', marginTop: '1rem' }}>
-    {!disabled && answers.length > 0 && (
-      <FormGroup>
-        {answers.map((answer, index) => (
-          <FormControlLabel key={index} value={answer} control={<Checkbox color='success' />} label={answer} />
-        ))}
-      </FormGroup>
-    )}
-    {disabled && (
-      <>
-        {list.map((item, index) => (
-          <FormControlLabel
-            key={index}
-            disabled={disabled}
-            control={<Checkbox color='success' />}
-            label={
-              <Input
-               placeholder='Ответ'
-               value={item[0] || ''}
-               onChange={(e) => handleUpdateAnswer(index, e.target.value)}
-              />
-            }
-          />
-        ))}
-        <FormControlLabel
-          disabled={disabled}
-          control={<Checkbox color='success' />}
-          label={
-            <Button color='success' variant="text" onClick={handleAddAnswer} >
-              Добавить вариант
-            </Button>
-          }
-        />
-      </>
-    )}
-  </FormGroup>
+      {!disabled && answers.length > 0 && (
+        <FormGroup>
+          {answers.map((answer, index) => (
+            <FormControlLabel key={index} value={answer} control={<Checkbox color='success' />} label={answer} />
+          ))}
+        </FormGroup>
+      )}
+      {disabled && (
+        <>
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <Droppable droppableId="droppable">
+              {(provided) => (
+                <>
+                  {list.map((item, index) => (
+                    <Draggable key={index} draggableId={index.toString()} index={index}>
+                      {(provided) => (
+                        <FormControlLabel
+                          key={index}
+                          disabled={disabled}
+                          control={<Checkbox color='success' />}
+                          label={
+                            <Input
+                              placeholder='Ответ'
+                              value={item[0] || ''}
+                              onChange={(e) => handleUpdateAnswer(index, e.target.value)}
+                            />
+                          }
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+                  <FormControlLabel
+                    disabled={disabled}
+                    control={<Checkbox color='success' />}
+                    label={
+                      <Button color='success' variant="text" onClick={handleAddAnswer} >
+                        Добавить вариант
+                      </Button>
+                    }
+                  />
+                </>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </>
+      )}
+    </FormGroup>
   );
 };
 
